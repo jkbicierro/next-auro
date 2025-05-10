fe6600847c9a14cde7d040a721fa8b1918226ed9
Jeck <zrkeimperial@gmail.com>
2025-05-10 21:10:27 +0800

    ver0.1.15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ import { Ripple } from "@/components/magicui/ripple";
 import { TextAnimate } from "@/components/magicui/text-animate";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Github } from "lucide-react";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { Github, Link2 } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -14,27 +19,44 @@ import { toast } from "sonner";
 //import Image from "next/image";
 
 export default function Home() {
-    const router = useRouter();
-    const [ticketId, setTicketId] = useState("");
-
-    async function CheckTicket() {
-        if (!ticketId || isNaN(Number(ticketId))) {
-            toast.warning("The ticket ID must be a valid number");
-            return;
-        }
-        router.push(`/ticket/${ticketId}`);
-    }
     return (
         <>
             <nav className="z-50 fixed bg-background px-[20px] lg:px-[100px] xl:px-[150px] 2xl:px-[400px] h-[60px] w-full flex items-center justify-between border-b">
                 <div>Auro</div>
 
                 <div>
-                    <Button size={"icon"} variant={"ghost"} asChild>
-                        <Link href="https://github.com/jkbicierro/next-auro">
-                            <Github />
-                        </Link>
-                    </Button>
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <Button size={"icon"} variant={"ghost"}>
+                                <Github />
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-[200px] px-5 py-2">
+                            <div>
+                                <ul className="flex flex-col gap-2">
+                                    <Link
+                                        href={
+                                            "https://github.com/jkbicierro/express-auro"
+                                        }
+                                    >
+                                        <li className="hover:text-zinc-200 cursor-pointer flex items-center gap-2">
+                                            <Link2 size={14} />
+                                            express-auro
+                                        </li>
+                                    </Link>
+                                    <Link
+                                        href={
+                                            "https://github.com/jkbicierro/next-auro"
+                                        }
+                                    >
+                                        <li className="hover:text-zinc-200 cursor-pointer flex items-center gap-2">
+                                            <Link2 size={14} /> next-auro
+                                        </li>
+                                    </Link>
+                                </ul>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
                 </div>
             </nav>
             <main className="h-screen flex items-center justify-center relative overflow-hidden">
@@ -52,21 +74,11 @@ export default function Home() {
                         >
                             Where Access Meets Accountability
                         </TextAnimate>
+
                         <BlurFade delay={1}>
-                            <div className="flex gap-2 items-center">
-                                <Input
-                                    placeholder="Got a ticket? Paste it here"
-                                    className="w-[300px] bg-stone-950/70"
-                                    value={ticketId}
-                                    onChange={(e) =>
-                                        setTicketId(e.target.value)
-                                    }
-                                />
-                                <Button onClick={CheckTicket}>
-                                    View Ticket
-                                </Button>
-                            </div>
+                            <TicketInput />
                         </BlurFade>
+
                         <Link href={"/auth/login"}>
                             <TextAnimate
                                 animation="scaleUp"
@@ -87,5 +99,52 @@ export default function Home() {
                 </div>
             </main>
         </>
+    );
+}
+
+function TicketInput() {
+    const router = useRouter();
+    const [ticketId, setTicketId] = useState("");
+
+    async function CheckTicket() {
+        if (!ticketId) {
+            toast.warning("Please enter a ticket ID");
+            return;
+        }
+
+        try {
+            const res = await fetch(
+                `${process.env.NEXT_PUBLIC_SERVER_URL}/api/ticket/show`,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    credentials: "include",
+                    body: JSON.stringify({ ticket_id: ticketId }),
+                }
+            );
+
+            const data = await res.json();
+            if (res.ok) {
+                toast.success(data.message);
+                router.push(`/ticket/${ticketId}`);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    return (
+        <div className="flex gap-2 items-center">
+            <Input
+                placeholder="Got a ticket? Paste it here"
+                className="w-[300px] bg-stone-950/70"
+                value={ticketId}
+                onChange={(e) => setTicketId(e.target.value)}
+            />
+            <Button onClick={CheckTicket}>View Ticket</Button>
+        </div>
     );
 }
